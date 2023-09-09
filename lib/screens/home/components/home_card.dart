3a63ebf63e4e29adecc42b1ac4d842bbe07f5209
ahmermalik099 <ahmermalik099@gmail.com>
2023-09-10 01:39:42 +0500

    home

---
+++ lib/screens/home/components/home_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeCard extends StatelessWidget {
+  String name = '';
+  String image = '';
+  String route;
+
+  HomeCard({
+    required this.name,
+    required this.image,
+    required this.route
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.only(left: 16,top: 16,right: 6,bottom: 8),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, '/$route');
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.3,
+          width: MediaQuery.of(context).size.width * 0.4,
+          decoration: BoxDecoration(
+            color: Colors.cyanAccent.shade700,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              style: BorderStyle.solid,
+              width: 2,
+              color: Colors.black26,
+            ),
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                  'assets/$image',
+                  height: MediaQuery.of(context).size.height * 0.2,
+              ),
+              Text(name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.white,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_neighbour/screens/home/components/home_card.dart';
 import 'package:hello_neighbour/screens/home/components/showcase_slider.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,11 +8,23 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 40,),
-          ShowcaseSlider(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 40,),
+            ShowcaseSlider(),
+            GridView.count(
+              crossAxisCount: 2,
+              shrinkWrap: true,
+              children: [
+                HomeCard(name: 'Explore', image: '1.webp',route: 'explore'),
+                HomeCard(name: 'Chat', image: '2.webp', route: 'chat'),
+                HomeCard(name: 'Settings', image: '1.webp', route: 'userProfile'),
+                HomeCard(name: 'About App', image: '2.webp', route: 'onBoarding'),
+              ],
+            ),
+          ]
+        ),
       ),
     );
   }
